9132483895d19f03994fb7f48893e04892901714
choi-won-ik <whwwhs7837@gmail.com>
2025-03-18 11:24:50 +0900

    test

--- src/main/java/com/example/demo/utils/JwtUtil.java
+++ src/main/java/com/example/demo/utils/JwtUtil.java
@@ -14,18 +14,10 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    @Value("${jwt.secret}")
-    private String secret;
-    private static String SECRET;
-    @Value("${jwt.expired}")
-    private int exp;
-    private static int EXP;
 
-    @PostConstruct
-    public void init() {
-        SECRET = secret;
-        EXP = exp;
-    }
+    private static String SECRET="abcdefghijklmnopqrstuvwxyz0123456789abcdefghijklmnopqrstuvwxyz0123456789";
+    private static int EXP=99999;
+
 
     public static Member getMember(String token) {
         try {
